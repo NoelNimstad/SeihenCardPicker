86b27fd1411d2780e7544c31f09984b87badb86a
basyl <69351096+thebasyl@users.noreply.github.com>
2024-02-17 10:24:26 +0100

    initial

---
+++ script.js
@@ -0,0 +1,34 @@
+const cards = document.querySelectorAll(".card");
+cards.forEach(card => 
+{
+    card.addEventListener("click", function () 
+    {
+        const currentCategory = this.closest(".card-category");
+        currentCategory.querySelectorAll(".card").forEach(card => 
+        {
+            card.classList.remove("active");
+        });
+        this.classList.add("active");
+    });
+});
+
+let rot = 0;
+const randomSelectButton = document.getElementById("random-select");
+randomSelectButton.addEventListener("click", function () 
+{
+    const categories = document.querySelectorAll(".card-category");
+
+    rot += 360;
+    randomSelectButton.style.rotate = rot + "deg";
+
+    categories.forEach(category => 
+    {
+        const cardsInCategory = category.querySelectorAll(".card");
+        const randomIndex = Math.floor(Math.random() * cardsInCategory.length);
+        cardsInCategory.forEach(card => 
+        {
+            card.classList.remove("active");
+        });
+        cardsInCategory[randomIndex].classList.add("active");
+    });
+});
